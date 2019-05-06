dcdeadd5176a7d57bc4672257b1db67a728fb50c
sokhadze <lerisokhadze@gmail.com>
2019-05-06 17:43:02 +0400

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,9 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'test',
+    loadChildren: './test/test.module#TestPageModule' }
 ];
 
 @NgModule({
